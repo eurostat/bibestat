025cb84083904ec8c699668efd09c53a382c2c01
gjacopo <gjacopo@gmail.com>
2020-09-24 01:38:06 +0200

    test action

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-BIBFILE=$1
-echo $BIBFILE
-
 export LANG=en_US.UTF-8
 
+#BIBFILE=$1
 #pandoc-citeproc --bib2json $GITHUB_WORKSPACE/$BIBFILE > $GITHUB_WORKSPACE/${BIBFILE%.*}.json
 
-# pandoc-citeproc --bib2json $1 `basename $1 .bib`.json
-pandoc-citeproc --bib2json eurostatref.bib > eurostatref.json
+pandoc-citeproc --bib2json /github/workspace/eurostatref.bib > /github/workspace/eurostatref.json
