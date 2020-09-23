4d0c6c30f062d869d5e5929421596e73d989ef8a
gjacopo <gjacopo@gmail.com>
2020-09-24 00:38:26 +0200

    testing action

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
-echo "WTF $1"
-echo "WTF ${GITHUB_WORKSPACE}"
-pandoc-citeproc --bib2json ${GITHUB_WORKSPACE}/$1.bib > ${GITHUB_WORKSPACE}/$1.json
+# echo "WTF $1"
+# echo "WTF ${GITHUB_WORKSPACE}"
+# pandoc-citeproc --bib2json ${GITHUB_WORKSPACE}/$1.bib > ${GITHUB_WORKSPACE}/$1.json
+pandoc-citeproc --bib2json ./temp/eurostatref.bib > ./temp/eurostatref.json
