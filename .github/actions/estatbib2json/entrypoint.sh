df45a718ed0a0e47aa39281ea83c7c1258141d0b
Jacopo Grazzini <gjacopo@gmail.com>
2020-09-23 20:22:32 +0200

    Update entrypoint.sh

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
-pandoc-citeproc --bib2json ${GITHUB_WORKSPACE}/$1.bib > ${GITHUB_WORKSPACE}/$1.json
+echo "Hello $1"
+# pandoc-citeproc --bib2json ${GITHUB_WORKSPACE}/$1.bib > ${GITHUB_WORKSPACE}/$1.json
