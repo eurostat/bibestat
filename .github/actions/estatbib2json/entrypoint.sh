2fa3e8687b8e3b0082ac6bd38fb7a8ef70039c63
gjacopo <gjacopo@gmail.com>
2020-09-24 01:09:08 +0200

    Update entrypoint.sh

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+BIBFILE=$1
+echo $BIBFILE
+
 export LANG=en_US.UTF-8
-# echo "WTF $1"
-# echo "WTF ${GITHUB_WORKSPACE}"
-# pandoc-citeproc --bib2json ${GITHUB_WORKSPACE}/$1.bib > ${GITHUB_WORKSPACE}/$1.json
-pandoc-citeproc --bib2json $1 #eurostatref.bib 
-# > eurostatref.json
+
+pandoc-citeproc --bib2json $GITHUB_WORKSPACE/$BIBFILE > $GITHUB_WORKSPACE/${BIBFILE%.*}.json
+
+# pandoc-citeproc --bib2json $1 `basename $1 .bib`.json
+# pandoc-citeproc --bib2json eurostatref.bib > eurostatref.json
