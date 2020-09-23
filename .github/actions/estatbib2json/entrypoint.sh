3f8e77cc8158e6159417dda4e6ad6ed2e8ae1f98
gjacopo <gjacopo@gmail.com>
2020-09-24 00:46:35 +0200

    dumb entry point

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -3,4 +3,5 @@ export LANG=en_US.UTF-8
 # echo "WTF $1"
 # echo "WTF ${GITHUB_WORKSPACE}"
 # pandoc-citeproc --bib2json ${GITHUB_WORKSPACE}/$1.bib > ${GITHUB_WORKSPACE}/$1.json
-pandoc-citeproc --bib2json ./temp/eurostatref.bib > ./temp/eurostatref.json
+pandoc-citeproc --bib2json eurostatref.bib 
+# > eurostatref.json
