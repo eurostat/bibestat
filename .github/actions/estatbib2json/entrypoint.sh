289f4ed23e3256251c55bef9717d88eed44f09f6
gjacopo <gjacopo@gmail.com>
2020-09-24 01:24:40 +0200

    copy in container

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -5,7 +5,7 @@ echo $BIBFILE
 
 export LANG=en_US.UTF-8
 
-pandoc-citeproc --bib2json $GITHUB_WORKSPACE/$BIBFILE > $GITHUB_WORKSPACE/${BIBFILE%.*}.json
+#pandoc-citeproc --bib2json $GITHUB_WORKSPACE/$BIBFILE > $GITHUB_WORKSPACE/${BIBFILE%.*}.json
 
 # pandoc-citeproc --bib2json $1 `basename $1 .bib`.json
-# pandoc-citeproc --bib2json eurostatref.bib > eurostatref.json
+pandoc-citeproc --bib2json eurostatref.bib > eurostatref.json
