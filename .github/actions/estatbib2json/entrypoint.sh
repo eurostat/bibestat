854bf7f06aa41ba308d9bb27f2d8a7528dd39bc8
Jacopo Grazzini <gjacopo@gmail.com>
2020-09-24 09:02:09 +0200

    Update entrypoint.sh

--- .github/actions/estatbib2json/entrypoint.sh
+++ .github/actions/estatbib2json/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+# Script for producing a BibJSON file from a BibTeX source using pandoc-citeproc
+# The source is represented by its basename and parsed as an argument ($1) 
+# to the script, e.g. the script is equivalent to:
+# > pandoc-citeproc --bib2json $1.bib > $1.json
+# Note that the locale env variable LANG is also set to UTF-8
+# see https://github.com/jgm/pandoc-citeproc
+
+# credits: grazzja (Eurostat, Unit B1)
+# since: 09/20
+
+BIBFILE=$1
+if [ ${BIBFILE##*.} = bib ]; then
+  BIBFILE=${BIBFILE%%.*}
+fi
+
 export LANG=en_US.UTF-8
-#BIBFILE=$1
-#pandoc-citeproc --bib2json $GITHUB_WORKSPACE/$BIBFILE > $GITHUB_WORKSPACE/${BIBFILE%.*}.json
-pandoc-citeproc --bib2json eurostatref.bib > eurostatref.json
-#echo `ls $GITHUB_WORKSPACE`
+pandoc-citeproc --bib2json $BIBFILE.bib > $BIBFILE.json
+#pandoc-citeproc --bib2json $GITHUB_WORKSPACE/$BIBFILE.bib > $GITHUB_WORKSPACE/$BIBFILE.json
